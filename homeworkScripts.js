e655215a4744cdf7dc7d1f9019eb383b2a645780
liubovpotapova <liubov.potapova@gmail.com>
2022-05-13 00:06:33 +0300

    switch function added

--- homeworkScripts.js
+++ homeworkScripts.js
@@ -1,2 +1,21 @@
 let UserCatName = prompt("А как зовут вашего пирожочка?, Введите имя туть:3 ");
-alert( `Эй, ${UserCatName}, кажется твой хуман настоящий пончик` );
+// if (UserCatName == Рыжий) {
+//     alert( 'Опа, тезки!' );
+//    } else {
+//     alert( `Эй, ${UserCatName}, кажется твой хуман настоящий пончик` );
+//    }
+
+// let a = UserCatName;
+   switch (UserCatName) {
+    case 'Рыжий':
+    alert( 'О, тезка!' );
+    break;
+    case 'Барсик':
+    alert( 'Хороший мальчик!' );
+    break;
+    case 'Васька':
+    alert( 'Свой парень!' );
+    break;
+    default:
+    alert( `Эй, ${UserCatName}, кажется твой хуман настоящий пончик` );
+   } 
